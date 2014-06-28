6d45c557fe6bd694bcf777c3c8e6136954d5ddb8
Adrian Kuendig <adriankue@gmail.com>
2014-06-28 14:59:21 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+# coding=utf-8
 from distutils.core import setup
 
 setup(
     name="aplus",
-    version="0.10",
-    packages=['aplus']
+    version="0.11",
+    packages=['aplus'],
+    author=u"Adrian Kündig",
+    author_email="adriankue@gmail.com"
 )
