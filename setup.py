5e34e90b5591bf46a6211ee4b40d170c188e94f8
Michael Tiller <michael.tiller@gmail.com>
2023-05-20 06:32:44 -0400

    fix: include LICENSE file in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="aplus",
     version="0.11.0",
     packages=['aplus'],
+    license_files = ('LICENSE.md',),
     author=u"Michael M. Tiller",
     author_email="michael.tiller@gmail.com"
 )
